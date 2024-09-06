85255729e128cf728bbb3a02aee19cc6080c8311
rahulOFS <rahul.soni27.techforceglobal@gmail.com>
2024-09-06 14:25:18 +0530

    ui branch

---
+++ ABCDUI.swift
@@ -0,0 +1,20 @@
+//
+//  ABCDUI.swift
+//  AuthenticatorDemo
+//
+//  Created by Apple on 06/09/24.
+//
+
+import SwiftUI
+
+struct ABCDUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ABCDUI_Previews: PreviewProvider {
+    static var previews: some View {
+        ABCDUI()
+    }
+}
